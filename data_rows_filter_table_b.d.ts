735661058d0615e4fddd0a8925c77031ce24f22a
Brian Takita <brian.takita@gmail.com>
2020-10-02 16:44:00 -0700

    type updates

--- data_rows_filter_table_b.d.ts
+++ data_rows_filter_table_b.d.ts
@@ -1,3 +1,3 @@
 import type { row_type } from './row_type';
-export declare function rows_data_filter_table_b<I extends row_type>(ctx?: any): any;
+export declare function rows_data_filter_table_b<I extends row_type>(ctx?: any): import("@ctx-core/store").Readable<Record<string, I[]>>;
 export declare const b__table__filter__rows__data: typeof rows_data_filter_table_b;
--- reverse_columns_b.d.ts
+++ reverse_columns_b.d.ts
@@ -1,2 +1,2 @@
-export declare const reverse_columns_b: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => any;
-export declare const b__reverse__columns: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => any;
+export declare const reverse_columns_b: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => import("@ctx-core/store").Readable<string[]>;
+export declare const b__reverse__columns: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => import("@ctx-core/store").Readable<string[]>;
