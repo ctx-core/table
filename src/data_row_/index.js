cd2a482b2e98402254d8b4e3ab523ba3f49955ca
Brian Takita <brian.takita@gmail.com>
2023-03-07 17:26:47 -0500

    data_row_: performance:
    	get,set: extract to top-level functions to reduce dynamic closure scope
    	+ datum_a_M_column_M_row_idx: WeakMap to store datum_a->column_M_row_idx: supports get,set extraction to top-level

--- src/data_row_/index.js
+++ src/data_row_/index.js
@@ -1,3 +1,8 @@
+const datum_a_M_column_M_row_idx__sym = Symbol.for('datum_a_M_column_M_row_idx')
+if (!globalThis[datum_a_M_column_M_row_idx__sym]) {
+	globalThis[datum_a_M_column_M_row_idx__sym] = new WeakMap()
+}
+const datum_a_M_column_M_row_idx = globalThis[datum_a_M_column_M_row_idx__sym]
 /**
  * @param {unknown[]}datum_a
  * @param {column_M_row_idx_T}[column_M_row_idx]
@@ -8,26 +13,26 @@ export function data_row_(
 	datum_a,
 	column_M_row_idx
 ) {
-	return new Proxy(datum_a, {
-		get(target, name) {
-			const column_idx = column_M_row_idx.get(name)
-			if (column_idx != null) {
-				return datum_a[column_idx]
-			} else {
-				return datum_a[name]
-			}
-		},
-		set(target, prop, val) {
-			const column_idx = column_M_row_idx.get(name)
-			if (column_idx != null) {
-				datum_a[column_idx] = val
-			} else {
-				datum_a[name] = val
-			}
-		}
-	})
+	datum_a_M_column_M_row_idx.set(datum_a, column_M_row_idx)
+	return new Proxy(datum_a, { get, set })
 }
 export {
 	data_row_ as _row_proxy,
 	data_row_ as _proxy__row,
+}
+function get(target, name) {
+	const column_idx = datum_a_M_column_M_row_idx.get(target).get(name)
+	if (column_idx != null) {
+		return target[column_idx]
+	} else {
+		return target[name]
+	}
+}
+function set(target, prop, val) {
+	const column_idx = datum_a_M_column_M_row_idx.get(target).get(name)
+	if (column_idx != null) {
+		target[column_idx] = val
+	} else {
+		target[name] = val
+	}
 }
