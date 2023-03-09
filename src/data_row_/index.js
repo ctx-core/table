095355777d9f05f2122569b66f7fab10f39354fe
Brian Takita <brian.takita@gmail.com>
2023-03-08 19:20:27 -0500

    + length_sym
    data_row_: data_row[length_sym]: returns target array length: fix: collision with length column values

--- src/data_row_/index.js
+++ src/data_row_/index.js
@@ -1,3 +1,4 @@
+export const length_sym = Symbol.for('length')
 const datum_a_M_column_M_row_idx__sym = Symbol.for('datum_a_M_column_M_row_idx')
 if (!globalThis[datum_a_M_column_M_row_idx__sym]) {
 	globalThis[datum_a_M_column_M_row_idx__sym] = new WeakMap()
@@ -5,7 +6,7 @@ if (!globalThis[datum_a_M_column_M_row_idx__sym]) {
 const datum_a_M_column_M_row_idx = globalThis[datum_a_M_column_M_row_idx__sym]
 /**
  * @param {unknown[]}datum_a
- * @param {column_M_row_idx_T}[column_M_row_idx]
+ * @param {column_M_row_idx_T}column_M_row_idx
  * @returns {data_row_T<unknown>}
  * @private
  */
@@ -13,6 +14,9 @@ export function data_row_(
 	datum_a,
 	column_M_row_idx
 ) {
+	if (!column_M_row_idx) {
+		throw new Error('data_row_: column_M_row_idx: missing')
+	}
 	datum_a_M_column_M_row_idx.set(datum_a, column_M_row_idx)
 	return new Proxy(
 		/** @type {data_row_T<unknown>} */datum_a,
@@ -25,10 +29,13 @@ export {
 }
 /**
  * @param {data_row_T}target
- * @param {string}prop_name
+ * @param {string|symbol}prop_name
  * @returns {unknown}
  */
 function get(target, prop_name) {
+	if (prop_name === length_sym) {
+		return target.length
+	}
 	const column_idx = datum_a_M_column_M_row_idx.get(target).get(prop_name)
 	if (column_idx != null) {
 		return target[column_idx]
