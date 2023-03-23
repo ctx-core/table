ad0a8051d5d9f42408b63440ed50818bdf7a889a
Brian Takita <brian.takita@gmail.com>
2023-03-23 11:22:02 -0400

    data_row_tuple_T: generics: object: fix: use given object type

--- src/_types/data_row_T.d.ts
+++ src/_types/data_row_T.d.ts
@@ -1,11 +1,7 @@
 import type { data_row_o_T } from './data_row_o_T'
-import type { data_row__sym_props_o_T } from './data_row__sym_props_o_T'
 export type data_row_T<
 	ColDefs extends (([string, any][])|any[]|object) = ([string, any][])|any[]|object
 > = data_row_tuple_T<ColDefs>&data_row_o_T<ColDefs>
-	// ColDefs extends [string, any][]
-	// ? data_row_tuple_T<ColDefs>&data_row_o_T<ColDefs>
-	// : data_row_tuple_T<ColDefs>&data_row__sym_props_o_T<ColDefs>
 export type Row<
 	ColDefs extends (([string, any][])|any[]|object) = ([string, any][])|any[]|object
 > = data_row_T<ColDefs>
@@ -16,4 +12,6 @@ export type data_row_tuple_T<
 	? { [K in keyof ColDefs]:ColDefs[K][1] }
 	: ColDefs extends any[]
 		? ColDefs
-		: string[]
+		: ColDefs extends object
+			? ColDefs
+			: string
--- src/_types/data_row_o_T.d.ts
+++ src/_types/data_row_o_T.d.ts
@@ -1,5 +1,12 @@
 import type { tuple__o_T } from './tuple__o_T'
 import { data_row__sym_props_o_T } from './data_row__sym_props_o_T'
 export type data_row_o_T<
+	ColDefs extends (([string, any][])|any[]|object) = ([string, any][])|any[]|object
+> =
 	ColDefs extends [string, any][]
-> = tuple__o_T<ColDefs, 0, 1>&data_row__sym_props_o_T<ColDefs>
+	? tuple__o_T<ColDefs, 0, 1>&data_row__sym_props_o_T<ColDefs>
+	: ColDefs extends any[]
+		? {}
+		: ColDefs extends object
+			? ColDefs
+			: never
